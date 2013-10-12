c4bb9adee836c056608565ce3140446f6712c51f
Joshua Hawxwell <m@hawx.me>
2013-10-12 20:21:46 +0100

    Adds features. Lots of them...
    - Deserialise types
    - Deserialise other objects
    - Deserialise list of other objects

--- lib/serialisable.rb
+++ lib/serialisable.rb
@@ -3,32 +3,89 @@ require 'nokogiri'
 module Serialisable
   def root(selector)
     @__root = selector
+    @__element = {}
     @__elements = {}
   end
 
-  def element(name, selector)
-    @__elements[name] = selector
+  def element(name, selector, type=String)
+    @__element[name] = [selector, type]
+  end
+
+  def elements(name, klass)
+    @__elements[name] = [klass]
   end
 
   def deserialise(xml)
-    doc = Nokogiri::XML(xml).children.find {|node| node.name == @__root }
+    __deserialise Nokogiri::XML(xml)
+  end
 
-    attrs = @__elements.map do |name, selector|
-      value = doc.children.find {|node| node.name == 'node' }.children.to_s
+  private
 
-      [name, value]
+  def __deserialise_all(doc)
+    doc = doc.children.find_all {|node| node.name == @__root }
+
+    attrs_list = doc.map do |node|
+      @__element.map do |name, (selector, type)|
+        value = node.children.find {|node| node.name == selector }.children.to_s
+
+        if type.respond_to?(:parse)
+          value = type.parse(value)
+        end
+
+        [name, value]
+      end
+    end
+
+    objs = []
+    attrs_list.each do |attrs|
+      attrs = Hash[attrs]
+
+      attrs.each do |key, value|
+        define_method key do
+          instance_variable_get(:@__serialisable_attrs)[key]
+        end
+      end
+
+      obj = new
+      obj.instance_variable_set(:@__serialisable_attrs, attrs)
+      objs << obj
+    end
+
+    objs
+  end
+
+  def __deserialise(doc)
+    doc = doc.children.find {|node| node.name == @__root }
+
+    attrs = @__elements.map do |name, (klass)|
+      [name, klass.send(:__deserialise_all, doc)]
+    end
+
+    attrs += @__element.map do |name, (selector, type)|
+      if selector.respond_to?(:__deserialise, true)
+        [name, selector.send(:__deserialise, doc)]
+      else
+        value = doc.children.find {|node| node.name == selector }.children.to_s
+
+        if type.respond_to?(:parse)
+          value = type.parse(value)
+        end
+
+        [name, value]
+      end
     end
 
     attrs = Hash[attrs]
 
     attrs.each do |key, value|
       define_method key do
-        instance_variable_get(:@attrs)[key]
+        instance_variable_get(:@__serialisable_attrs)[key]
       end
     end
 
     obj = new
-    obj.instance_variable_set(:@attrs, attrs)
+    obj.instance_variable_set(:@__serialisable_attrs, attrs)
     obj
   end
+
 end
--- spec/serialisable_spec.rb
+++ spec/serialisable_spec.rb
@@ -2,22 +2,129 @@ require_relative 'spec_helper'
 
 describe Serialisable do
 
-  subject {
-    Class.new {
-      extend Serialisable
-
-      root 'root'
-      element :node, 'node'
-    }
-  }
-
   describe '#deserialise' do
-    let(:xml) {
-      '<?xml version="1.0" encoding="utf-8"?><root><node>value</node></root>'
+    describe 'with one node' do
+
+      subject {
+        Class.new {
+          extend Serialisable
+
+          root 'root'
+          element :node, 'node'
+        }
+      }
+
+      let(:xml) {
+        '<?xml version="1.0" encoding="utf-8"?><root><node>value</node></root>'
+      }
+
+      it 'takes xml and returns an object' do
+        subject.deserialise(xml).node.must_equal 'value'
+      end
+    end
+
+    describe 'with an element to parse' do
+
+      subject {
+        require 'time'
+
+        Class.new {
+          extend Serialisable
+
+          root 'root'
+          element :time, 'time', Time
+        }
+      }
+
+      let(:xml) {
+        '<?xml version="1.0" encoding="utf-8"?><root><time>2013-07-04T13:23:34Z</time></root>'
+      }
+
+      it 'takes xml and returns an object, with types parsed' do
+        subject.deserialise(xml).time.must_equal Time.utc(2013, 7, 4, 13, 23, 34)
+      end
+    end
+  end
+
+  describe 'with a nested serialisable' do
+
+    subject {
+      Class.new {
+        extend Serialisable
+
+        root 'songs'
+        element :song, Class.new {
+          extend Serialisable
+
+          root 'song'
+          element :artist, 'artist'
+          element :name, 'name'
+        }
+      }
     }
 
-    it 'takes xml and returns an object' do
-       subject.deserialise(xml).node.must_equal 'value'
+    let(:xml) {
+      <<EOS
+<?xml version="1.0" encoding="utf-8"?>
+<songs>
+  <song>
+    <artist>Arctic Monkeys</artist>
+    <name>505</name>
+  </song>
+</songs>
+EOS
+    }
+
+    it 'deserialises the nested object correctly' do
+      result = subject.deserialise(xml)
+
+      result.song.artist.must_equal 'Arctic Monkeys'
+      result.song.name.must_equal '505'
+    end
+  end
+
+  describe 'with a list of nested objects' do
+    subject {
+      Class.new {
+        extend Serialisable
+
+        root 'songs'
+        elements :songs, Class.new {
+          extend Serialisable
+
+          root 'song'
+          element :artist, 'artist'
+          element :name, 'name'
+        }
+      }
+    }
+
+    let(:xml) {
+      <<EOS
+<?xml version="1.0" encoding="utf-8"?>
+<songs>
+  <song>
+    <artist>Arctic Monkeys</artist>
+    <name>505</name>
+  </song>
+  <song>
+    <artist>Aphex Twin</artist>
+    <name>Windowlicker</name>
+  </song>
+</songs>
+EOS
+    }
+
+    it 'takes xml and returns an object with a list of nested objects' do
+      result = subject.deserialise(xml)
+
+      result.songs.length.must_equal 2
+
+      result.songs[0].artist.must_equal 'Arctic Monkeys'
+      result.songs[0].name.must_equal '505'
+
+      result.songs[1].artist.must_equal 'Aphex Twin'
+      result.songs[1].name.must_equal 'Windowlicker'
     end
   end
 end
